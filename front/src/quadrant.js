e0760ab7e82a3982a00a79246317d21c372daa5b
Bruna Pereira <ppbruna2@gmail.com>
2017-05-30 23:48:39 -0300

    habemus handle click

--- front/src/quadrant.js
+++ front/src/quadrant.js
@@ -1,4 +1,9 @@
 import React from 'react' 
 
-export default ({ name }) => (<div className= {"col-1-2 quadrant " + name } >
-</div>)
+const handleClick = name => {
+  console.log('funciona', name)  
+}
+
+export default ({ name }) => 
+  <div className={"col-1-2 quadrant " + name } onClick={handleClick.bind(this, name)}>
+  </div>
