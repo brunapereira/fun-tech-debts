343c33934be5b5ebf0416f8a1915f7ea38381966
Bruna Pereira <ppbruna2@gmail.com>
2017-05-31 10:55:21 -0300

    adds title to quadrants

--- front/src/components/quadrant.js
+++ front/src/components/quadrant.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addCard } from '../actions'
 
-const Quadrant = ({ name, key, addCard, cards }) => 
+const Quadrant = ({ name, addCard, cards, title }) => 
   <div className={"col-1-2 quadrant " + name } onClick={() => addCard(name)}>
     <div className="grid">
+      <p>{title}</p>
       { cards.map((card, index) =><div className="col-1-3"><div className="card" id={index}>card</div></div>) }
     </div>
   </div>
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    cards: state[ownProps.name].cards
+    cards: state[ownProps.name].cards,
+    title: state[ownProps.name].title
   }
 }
 
