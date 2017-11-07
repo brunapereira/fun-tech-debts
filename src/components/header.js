a1c60e3f5f06f0b9c2c1af81864fde8b6724aa18
Bruna Pereira <ppbruna2@gmail.com>
2017-11-07 17:34:28 -0200

    removes quadrant border overlap

--- src/components/header.js
+++ src/components/header.js
@@ -2,4 +2,4 @@ import React from 'react'
 
 import './header.less'
 
-export default (props) => <header className='header'><h1>Fun Tech Debt</h1></header>
+export default (props) => <header className='header'><h1>Fun Tech Debts</h1></header>
