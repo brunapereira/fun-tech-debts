96949b9da2ddb62f46323477ea333814dede22e9
Bruna Pereira <ppbruna2@gmail.com>
2017-11-07 14:22:22 -0200

    sets up webpack for local env

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,40 @@
-var path = require('path')
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const extractLess = new ExtractTextPlugin({
+    filename: "index.css"
+})
+const htmlWebpack = new HtmlWebpackPlugin({
+	template: `${__dirname}/src/index.html`
+})
+const nodeEnv = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+  }
+})
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.join(__dirname, '../back/public'),
+    path: path.join(__dirname, './build'),
     publicPath: '',
     filename: 'main.js'
   },
+  plugins: [
+    extractLess,
+		htmlWebpack,
+    nodeEnv
+  ],
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } },
-      { test: /\.less$/, use: [ 'style-loader', 'css-loader', 'less-loader' ] }
+      { test: /\.jsx?$/, exclude: /(node_modules)/, loader: "babel-loader", query: { presets: ['react', 'es2015']} },
+        { test: /\.scss$/, use: extractLess.extract({ use: [ { loader: "css-loader"}, { loader: "sass-loader" } ], fallback: "style-loader" })},
+        { test: /\.css$/, use: extractLess.extract({ use: [ { loader: "css-loader", options: { modules: true, sourceMap: true, importLoaders: 1, localIdentName: "[name]--[local]--[hash:base64:8]" }}, { loader: "sass-loader" } ], fallback: "style-loader" })},
+        { test: /\.less$/, use: extractLess.extract({ use: [ { loader: "css-loader"}, { loader: "less-loader"} ], fallback: "style-loader" })},
+      { test: /\.(eot|woff|woff2|ttf|png|jpe?g|gif)(\?\S*)?$/, loader: 'url?limit=100000&name=[name].[ext]' },
+      { test: /\.svg/, use: { loader: 'svg-url-loader', options: {} }}
     ]  
   }
 }
