6dbddd18d6b054f3fab66d3ed2447cbc2dbc341d
Bruna Pereira <ppbruna2@gmail.com>
2017-11-08 14:39:08 -0200

    creates edit and done button to cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './card.less'
 
-export default ({index, quadrant}) => <div className="col-1-3"><div className={"card " + quadrant}></div></div>
+class Card extends Component {
+  constructor() {
+    super()
+    this.state = { isEditing: false }
+  }
+
+  handleEdit() {
+    this.setState({ isEditing: !this.state.isEditing })
+  }
+
+  render() {
+    return (
+      this.state.isEditing
+      ? (
+        <div className='col-1-3'>
+          <div className={'card ' + this.props.quadrant}>
+            <textarea></textarea>
+            <button className='done' onClick={this.handleEdit.bind(this)}>done</button>
+          </div>
+        </div>
+      )
+      : (
+        <div className='col-1-3'>
+          <div className={'card ' + this.props.quadrant}>
+            <a className='pencil' onClick={this.handleEdit.bind(this)}><i className='fa fa-pencil'></i></a>
+          </div>
+        </div>
+      )
+    )
+  }
+}
+
+export default Card
