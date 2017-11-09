270aa2bbacf7a40e05dda24132be52394ee99f59
Bruna Pereira <ppbruna2@gmail.com>
2017-11-09 12:47:43 -0200

    extracts firebase config to new file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+	apiKey: "AIzaSyAj29WLtuqyPoX0T2RTufIPSFBBtGUsPMM",
+	authDomain: "fun-tech-debts.firebaseapp.com",
+	databaseURL: "https://fun-tech-debts.firebaseio.com",
+	projectId: "fun-tech-debts",
+	storageBucket: "fun-tech-debts.appspot.com",
+	messagingSenderId: "115519258808"
+}
+
